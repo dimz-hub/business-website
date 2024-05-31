d99a9183ddba60f90b7ae35f22c7d51be9fd61cd
heedrhiss <muyideenidris77@gmail.com>
2024-05-31 16:32:22 +0100

    merge

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer className="text-center font-bold">
+            &copy; 2024 | All rights reserved.
+        </footer>
+    )
+}
+
+export default Footer
---
+++ src/components/Services.jsx
@@ -0,0 +1,102 @@
+import Footer from "./Footer"
+
+function Services() {
+    return ( 
+        <div className="w-full h-full bg-slate-50">
+        <div className="mx-auto p-8 text-center">
+            <h1 className="text-4xl font-bold mb-5">Our Services</h1>
+            <div className="flex space-x-7 mb-5 justify-center">
+            <div className="flex flex-col items-center justify-start w-1/4 bg-green-200 p-6 rounded-xl">
+            <div className="w-20 h-20 my-3 rounded-lg"><img src="./images/unnamed.jpg" alt="services" /></div>
+            <p className="text-lg text-left">
+            <h1 className="text-lg font-bold text-center">ISO 20000</h1>
+                Business Platform Limited provides organizations with the resources and expertise they need to implement and maintain an ISO 20000-compliant service management system (SMS). Our range of services, including:
+                <ul className="list-disc pl-5 mt-1">
+                    <li>Training and certification</li>
+                    <li>Process consulting</li>
+                    <li>Implementation support</li>
+                    <li>Ongoing monitoring and improvement</li>
+                </ul>
+            </p>
+            </div>
+
+            <div className="flex flex-col items-center justify-start w-1/4 bg-green-200 p-6 rounded-xl">
+            <div className="w-20 h-20 my-3 rounded-lg"><img src="./images/unnamed.jpg" alt="services" /></div>
+            <p className="text-lg text-left">
+            <h1 className="text-lg font-bold text-center">ITIL services</h1>
+            Our ITIL services are IT services that are delivered in accordance with the Information Technology Infrastructure Library (ITIL) framework  through  a systematic approach to the delivery of IT services utilizing: 
+            <ul className="list-disc pl-5 mt-1">
+                    <li>Service strategy</li>
+                    <li>Service design</li>
+                    <li>Service transition</li>
+                    <li>Service operation</li>
+                    <li>Continual service improvement</li>
+                </ul>
+            </p>
+            </div>
+
+            <div className="flex flex-col items-center justify-start w-1/4 bg-green-200 p-6 rounded-xl">
+            <div className="w-20 h-20 my-3 rounded-lg"><img src="./images/unnamed.jpg" alt="services" /></div>
+            <p className="text-lg text-left">
+            <h1 className="text-lg font-bold text-center">Software requirements management (SRM)</h1>
+            Software requirements management (SRM) is a process for gathering, documenting, and managing the requirements for a software system. It is an essential part of the software development process, as it helps to ensure that the system meets the needs of the users and stakeholders.
+            Our SRM services help organizations to:
+                <ul className="list-disc pl-5 mt-1">
+                    <li>Gather and document requirements</li>
+                    <li>Manage requirements</li>
+                    <li>Communicate requirements</li>
+                    <li>Validate requirements</li>
+                    <li>Verify requirements</li>
+                </ul>
+            </p>
+            </div>
+
+
+            </div>
+            <div className="flex space-x-5 justify-center">
+
+            <div className="flex flex-col items-center justify-start w-1/4 bg-green-200 p-6 rounded-xl">
+            <div className="w-20 h-20 my-3 rounded-lg"><img src="./images/unnamed.jpg" alt="services" /></div>
+            <p className="text-lg text-left">
+            <h1 className="text-lg font-bold text-center">Project Management Services</h1>
+            With our In-house team of Project managers, we offer Project management services that help organizations to deliver projects successfully. These services can include:
+                <ul className="list-disc pl-5 mt-1">
+                    <li>Project planning</li>
+                    <li>Project execution</li>
+                    <li>Project monitoring and control</li>
+                    <li>Project closure</li>
+                </ul>
+            </p>
+            </div>
+
+            <div className="flex flex-col items-center justify-start w-1/4 bg-green-200 p-6 rounded-xl">
+            <div className="w-20 h-20 my-3 rounded-lg"><img src="./images/unnamed.jpg" alt="services" /></div>
+            <p className="text-lg text-left">
+            <h1 className="text-lg font-bold text-center">Software Testing Services</h1>
+            Our Software testing services are a broad range of services that help organizations to ensure the quality of their software. These services can include:
+                <ul className="list-disc pl-5 mt-1">
+                    <li>Functional testing</li>
+                    <li>Performance testing</li>
+                    <li>Security testing</li>
+                    <li>Usability testing</li>
+                    <li>Acceptance testing</li>
+                </ul>
+            </p>
+            </div>
+
+            <div className="flex flex-col items-center justify-start w-1/4 bg-[#DBE7C9] p-6 rounded-xl">
+            <div className="w-20 h-20 my-3 rounded-lg"><img src="./images/unnamed.jpg" alt="services" /></div>
+            <p className="text-lg text-left">
+            <h1 className="text-lg font-bold text-center">Application Software Development Service</h1>
+            Through our Application Software Development Service, we develop custom software applications using the flow of Business analysis; Design; Development; Testing; Deployment and Maintenance.
+            </p>
+            </div>
+            </div>
+            
+        </div>
+        <Footer/>
+        </div>
+    )
+}
+
+export default Services
